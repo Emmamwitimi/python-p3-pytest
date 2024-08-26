2222f074ea369fea2871800c5949abc33ae3053b
Emmaculate <emmamwitimi14@gmail.com>
2024-08-26 22:02:39 +0300

    pytest

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+# testing/test_not_none.py
 
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    assert return_not_none() is not None  # This assertion will pass if return_not_none does not return None
